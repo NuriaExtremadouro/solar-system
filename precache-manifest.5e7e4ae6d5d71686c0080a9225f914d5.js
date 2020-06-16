9fc653ce7a03bde32bea09685eef4116c53fc9e2
Nuria Extremadouro Mayoral <nuriaextremadouromayoral@MacBook-Pro-de-Nuria.local>
2020-06-16 23:58:44 +0200

    Updates

---
+++ precache-manifest.5e7e4ae6d5d71686c0080a9225f914d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85cdc6c4b6d5737142ed25b368bc84a",
+    "url": "/solar-system/index.html"
+  },
+  {
+    "revision": "d059e4f78eced879604f",
+    "url": "/solar-system/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca1b26d89591427e5f0b",
+    "url": "/solar-system/static/js/2.1809ccfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/solar-system/static/js/2.1809ccfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d059e4f78eced879604f",
+    "url": "/solar-system/static/js/main.b0fbe549.chunk.js"
+  },
+  {
+    "revision": "a29893af65b6ed9dfbe2",
+    "url": "/solar-system/static/js/runtime-main.cd6a56d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/solar-system/static/media/logo.5d5d9eef.svg"
+  }
+]);
